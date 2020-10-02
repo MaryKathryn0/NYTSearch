2535e1aa003ea5f410488c9866ff9598b6edbfab
Robert Grimes <rjgrimes@gmail.com>
2020-10-02 13:42:25 -0500

    Begun layout of script areas

---
+++ mainScript.js
@@ -0,0 +1,23 @@
+//#region Variable Declaration
+
+
+//#endregion
+
+//#region API Variables
+
+//#endregion
+
+
+//#region Function Definitions
+
+
+
+//#endregion
+
+
+
+//#region Event Listeners
+
+
+
+//#endregion
