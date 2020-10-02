0a0d6462c1d8df032bbc16944e5b4562fb0936ec
Robert Grimes <rjgrimes@gmail.com>
2020-10-02 13:53:43 -0500

    Begun planning of API calls

--- mainScript.js
+++ mainScript.js
@@ -4,7 +4,11 @@
 //#endregion
 
 //#region API Variables
-
+// articlesearch.json?q={query}&fq={filter}
+// Example Call
+// https://api.nytimes.com/svc/search/v2/articlesearch.json?q={election}&api-key=yourkey
+// {election} above is the term the user typed
+var nytAPIKey = "jHYddvB3BUPWEx0heZRUb0DUopijsuye";
 //#endregion
 
 
